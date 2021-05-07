2bbf39ebea6d2d3d7ea216f9798be8ddd3148e17
David Keller <kellerdb@gmail.com>
2021-05-07 12:59:05 -0700

    formatting

--- script.js
+++ script.js
@@ -4,23 +4,22 @@ let box = document.getElementById(`box`)
 
 submitButton.addEventListener(`click`, submit)
 
+repeat.focus()
+
 function submit() {
   let repeatValue = repeat.value.trim()
 
-  if (repeatValue != `` && !isNaN(repeatValue)) {
-    if (repeatValue > 500) {
-      box.innerHTML = `Times to repeat cannot be more than 500.`
-    }
-    else {
-      box.innerHTML = ``
+  if (repeatValue > 500) {
+    box.innerHTML = `Times to repeat cannot be more than 500.`
+  } else {
+    box.innerHTML = ``
 
-      for (let i = 1; i <= repeatValue; i++) {
-        let image = document.createElement(`img`)
-        image.src = `donkey-kong.png`
-        image.style.width = `${i}px`
+    for (let i = 1; i <= repeatValue; i++) {
+      let image = document.createElement(`img`)
+      image.src = `donkey-kong.png`
+      image.style.width = `${i}px`
 
-        box.appendChild(image)
-      }
+      box.appendChild(image)
     }
   }
 }
